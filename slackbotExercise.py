fac380708fc2ee65f0a0e1d32b0cc999f9bae606
Adam Reis <adamhreis@gmail.com>
2015-06-29 15:30:05 -0400

    prelim refactor to use slacker

--- slackbotExercise.py
+++ slackbotExercise.py
@@ -9,13 +9,11 @@ import os
 import datetime
 
 try:
-    SLACK_API_GET_TOKEN = os.environ['SLACK_API_GET_TOKEN']
-    SLACK_API_POST_TOKEN = os.environ['SLACK_API_POST_TOKEN']
+    SLACK_API_TOKEN = os.environ['SLACK_API_TOKEN']
 except ImportError:
-    from secrets import SLACK_API_GET_TOKEN, SLACK_API_POST_TOKEN
+    from secrets import SLACK_API_TOKEN
 
-POST_MESSAGE_BASE_URL = "https://makeschool.slack.com/services/hooks/slackbot"
-LIST_CHANNEL_BASE_URL = "https://slack.com/api/channels.list"
+SLACK = Slacker(SLACK_API_TOKEN)
 
 START_TIME = datetime.time(9,0,0)
 END_TIME = datetime.time(20,0,0)
@@ -91,45 +89,39 @@ STRETCHES = {
     },
 }
 
-def name_for_id(user_id):
-    params = {"token": SLACK_API_GET_TOKEN}
-    response = requests.get("https://slack.com/api/users.list", params=params)
-    users = response.json().get("members")
+def names_for_ids(user_ids):
+    names = []
+    user_ids = set(user_ids)
 
+    response = SLACK.users.list()
+    users = response.body.get('members')
     for user in users:
-        if user.get("id") == user_id:
-            return user.get("name")
+        if user.get("id") in user_ids:
+            names.append(user.get("name"))
 
 def users_in_channel(channel_name):
-    params = {"token": SLACK_API_GET_TOKEN}
-    response = requests.get(LIST_CHANNEL_BASE_URL, params=params)
-    channels = response.json().get("channels")
-    ids = []
+    response = SLACK.channels.list()
+    channels = response.body.get('channels')
     for channel in channels:
         if channel.get("name") == channel_name:
             ids = list(channel.get("members"))
-            break
-    return ids
+            return names_for_ids(ids)
 
 def random_user_mention(channel_name):
-    ids = users_in_channel(channel_name)
-    return "@" + name_for_id(random.choice(ids))
-
-def post_to_channel(channel_name, message):
-    params = {
-        "token": SLACK_API_POST_TOKEN,
-        "channel": "#" + channel_name
-    }
-    return requests.post(POST_MESSAGE_BASE_URL, params=params, data=message)
+    users = users_in_channel(channel_name)
+    return "@" + random.choice(users)
 
 def activity_and_sleep(channel_name, activities, time_interval):
-    if datetime.datetime.today().weekday() < 5 and START_TIME <= datetime.datetime.now().time() <= END_TIME: #weeekday between 9am EST and 5pm PST
+    weekday_condition = datetime.datetime.today().weekday() < 5
+    time_condition = START_TIME <= datetime.datetime.now().time() <= END_TIME
+    
+    if weekday_condition and time_condition : #weekday between 9am EST and 5pm PST
         activity = activities.get(random.choice(list(activities.keys())))
         victim = random_user_mention(channel_name)
         reps = random.randrange(*activity.get("rep_range"))
         message = "{} {}{} RIGHT NOW {}".format(reps, activity.get("units"), activity.get("name"), victim)
         print(message)
-        print(post_to_channel(channel_name, message))
+        print(SLACK.chat.post_message(channel_name, message))
 
     delay = int(random.randrange(*time_interval)/60.0)*60
     time.sleep(delay)
@@ -137,11 +129,11 @@ def activity_and_sleep(channel_name, activities, time_interval):
     activity_and_sleep(channel_name, activities, time_interval)
 
 def exercise():
-    thread = threading.Thread(target=activity_and_sleep, args=("exercise", EXERCISES, (240, 900)), kwargs={})
+    thread = threading.Thread(target=activity_and_sleep, args=("#exercise", EXERCISES, (240, 900)), kwargs={})
     thread.start()
 
 def stretch():
-    thread = threading.Thread(target=activity_and_sleep, args=("stretching", STRETCHES, (480, 1800)), kwargs={})
+    thread = threading.Thread(target=activity_and_sleep, args=("#stretching", STRETCHES, (480, 1800)), kwargs={})
     thread.start()
 
 if __name__ == "__main__":
